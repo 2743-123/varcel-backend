b456d2cf5d38552fa6d9c43635091637dcb9b656
2743-123 <2743-123>
2024-10-18 15:45:28 +0530

    test

--- api/index.js
+++ api/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 
-app.get("/",(req,res)=>{
-    res.send("varcel server is on")
-
-})
-
-app.listen(port,()=>{
-console.log(`server runnig on port ${port}`)
+app.get("/", (req, res) => {
+  res.send("varcel server is on");
 });
+
+app.listen(port, () => {
+  console.log(`server runnig on port ${port}`);
+});
+module.exports = app;
